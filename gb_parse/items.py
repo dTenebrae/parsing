fd4c5782b8bc66be5cb09866d3dfcfdc26b81ef7
dtenebrae <dtenebrae@gmail.com>
2020-12-07 09:59:55 +0300

    lesson5 commit №1

--- gb_parse/items.py
+++ gb_parse/items.py
@@ -10,3 +10,20 @@ class GbParseItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class HeadHuntersJobItem(scrapy.Item):
+    _id = scrapy.Field()
+    job_title = scrapy.Field()
+    salary = scrapy.Field()
+    description = scrapy.Field()
+    tags = scrapy.Field()
+    employer_url = scrapy.Field()
+
+
+class HeadHunterCompanyItem(scrapy.Item):
+    _id = scrapy.Field()
+    company_name = scrapy.Field()
+    company_url = scrapy.Field()
+    company_desc = scrapy.Field()
+    company_fields = scrapy.Field()
--- gb_parse/pipelines.py
+++ gb_parse/pipelines.py
@@ -6,8 +6,16 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from pymongo import MongoClient
 
 
 class GbParsePipeline:
+
+    def __init__(self):
+        self.db = MongoClient()['parser']
+
     def process_item(self, item, spider):
+
+        collection = self.db[spider.name]
+        collection.insert_one(item)
         return item
